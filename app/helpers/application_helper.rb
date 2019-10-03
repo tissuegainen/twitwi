e15cd102af30d420a86e33a538ae40d95cce6a95
tissuegainen <tissuegainen@icloud.com>
2019-10-04 00:26:27 +0900

    [コミットメッセージ]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def get_twitter_card_info(post)
+    twitter_card = {}
+    if post.present?
+      if post.id.present?
+        twitter_card[:url] = "https://twitwi.herokuapp.com/posts/#{post.id}"
+        twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/twitwi-production/images/#{post.id}.png"
+      else
+        twitter_card[:url] = 'https://twitwi.herokuapp.com/'
+        twitter_card[:image] = "https://raw.githubusercontent.com/tissuegainen/twitwi/master/app/assets/images/top.png"
+      end
+    else
+      twitter_card[:url] = 'https://twitwi.herokuapp.com/'
+      twitter_card[:image] = "https://raw.githubusercontent.com/tissuegainen/twitwi/master/app/assets/images/top.png"
+    end
+    twitter_card[:title] = "twitwi"
+    twitter_card[:card] = 'summary_large_image'
+    twitter_card[:description] = 'twitwiからのツイート'
+    twitter_card
+  end
 end
